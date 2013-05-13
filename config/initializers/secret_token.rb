3f2c52cbb1fc904d11bfaa6ba0e2f3436b53b220
Rafael Souza <rafael@maisweb.org>
2013-05-13 12:51:36 -0300

    Projeto rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GerenciaConfigMudanca::Application.config.secret_token = '0046b7803128eb638c32ebba04975627a231eded060de663c1bc6c4f37d11a05dd56eb35232b5a30a2bf3ea3f1bccd3f2e206aa4446e8510edd1661a99eb046b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GerenciaConfigMudanca::Application.config.session_store :cookie_store, key: '_gerencia-config-mudanca_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GerenciaConfigMudanca::Application.config.session_store :active_record_store
